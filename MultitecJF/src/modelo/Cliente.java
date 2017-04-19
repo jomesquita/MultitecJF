ccc740212a5bdb08131fa367c4acaff560e63bed
Multitec <Multitec@20.20.0.117>
2017-04-19 16:05:53 -0300

    metodo cliente - Fabrício

--- MultitecJF/src/modelo/Cliente.java
+++ MultitecJF/src/modelo/Cliente.java
@@ -5,10 +5,33 @@
  */
 package modelo;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Multitec JF
  */
 public class Cliente {
+    public static void main(String[] args){
+    String nome[] = new String[3];
+    boolean logico = false;
+    int escolha, i;
+    String pesquisar;
+    escolha = Integer.parseInt(JOptionPane.showInputDialog(nome));
+        
     
+    for(i=0; i<nome.length; i++){
+    nome[i] = JOptionPane.showInputDialog("Digite seu nome");
+    }
+    
+    
+    for(i=0; i<nome.length; i++){
+    System.out.println("Nome: "+nome[i]);
+    }
+    
+    pesquisar = JOptionPane.showInputDialog("Digite o nome que deseja pesquisar: ");
+    for(i=0; i<nome.length; i++){
+    }
 }
+}
+
