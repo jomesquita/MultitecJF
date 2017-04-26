ac7c6dc097bf7a0bb0252c63d712845ae52283a1
Multitec <Multitec@20.20.0.117>
2017-04-26 16:44:14 -0300

    Fabrício - metodo cliente, equipamentos

--- MultitecJF/src/modelo/Equipamentos.java
+++ MultitecJF/src/modelo/Equipamentos.java
@@ -10,5 +10,11 @@ package modelo;
  * @author Multitec JF
  */
 public class Equipamentos {
+    int codigo;
+    String nome;
     
+    void reservar(){
+        System.out.println("O código da reserva é: " +this.codigo);
+        System.out.println("A reserva é de: " +this.nome);
+    }
 }
